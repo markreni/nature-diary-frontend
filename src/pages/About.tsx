fa5dd117edb1b5044ce619369e646ba0d322eee9
renssim1 <mark.renssi@aalto.fi>
2025-11-24 16:07:21 +0200

    fix: fixed deleting observation; refactor: removed unnecessary imports

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,4 @@
-import { Container, Row, Col, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 
 const About = () => {
   return (
